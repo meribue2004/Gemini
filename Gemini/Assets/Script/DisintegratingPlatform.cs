5195cdba588f16d30ad032ef393c33f82ff55c98
Konouz Abdelaziz <konozabdelaziz1@gmail.com>
2023-11-30 08:55:47 +0200

    koki 3aw

--- Gemini/Assets/Script/DisintegratingPlatform.cs
+++ Gemini/Assets/Script/DisintegratingPlatform.cs
@@ -1,79 +1,24 @@
 using UnityEngine;
-
+using System.Collections;
 public class DisintegratingPlatform : MonoBehaviour
 {
-    public float disintegrationTime = 3f;      // Time it takes for the platform to disintegrate
-    public Material disintegratedMaterial;     // Material to apply after disintegration
-    public float hopDurationThreshold = 0.5f;  // Minimum time the player needs to be on the platform to trigger disintegration
+    private float fallDelay = 1f;
+    private float destroyDelay = 2f;
 
-    private Material originalMaterial;
-    private bool isDisintegrating = false;
-    private float timeOnPlatform = 0f;
+    [SerializeField] private Rigidbody2D rb;//reference platform's rigidbody2d
 
-    private void Start()
-    {
-        // Assuming the platform has a SpriteRenderer component
-        originalMaterial = GetComponent<SpriteRenderer>().material;
-    }
-
-    private void Update()
-    {
-        if (isDisintegrating)
-        {
-            Disintegrate();
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)//check if player collide with platform
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Player has landed on the platform
-            timeOnPlatform = 0f;
+            StartCoroutine(Fall());//cause fall
         }
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private IEnumerator Fall()
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            // Increment the time the player has been on the platform
-            timeOnPlatform += Time.deltaTime;
-
-            // If the player has been on the platform for the specified duration, start disintegration
-            if (timeOnPlatform >= hopDurationThreshold && !isDisintegrating)
-            {
-                StartDisintegration();
-            }
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            // Player has left the platform, reset the timer
-            timeOnPlatform = 0f;
-        }
-    }
-
-    private void StartDisintegration()
-    {
-        isDisintegrating = true;
-        Invoke("CompleteDisintegration", disintegrationTime);
-    }
-
-    private void Disintegrate()
-    {
-        // Implement your disintegration logic here
-        // For a simple example, you can change the material or scale of the platform over time
-        float lerpValue = Mathf.PingPong(Time.time, disintegrationTime) / disintegrationTime;
-        GetComponent<SpriteRenderer>().material.Lerp(originalMaterial, disintegratedMaterial, lerpValue);
-    }
-
-    private void CompleteDisintegration()
-    {
-        // Optionally, perform any cleanup or destruction logic here
-        Destroy(gameObject);
+        yield return new WaitForSeconds(fallDelay);//wait for a while before the platform falls
+        rb.bodyType = RigidbodyType2D.Dynamic;//falls
+        Destroy(gameObject, destroyDelay);//dont want platform to fall forever, destroy after a time
     }
 }
---
+++ Gemini/Assets/Script/lowOpacity.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lowOpacity : MonoBehaviour
+{
+    private SpriteRenderer mySprite;
+    private Color originalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+        originalColor = mySprite.color; // Store the original color
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // You can add any update logic here if needed
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SetOpacity(0.5f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ResetOpacity();
+        }
+    }
+
+    private void SetOpacity(float opacity)
+    {
+        Color newColor = originalColor;
+        newColor.a = opacity; // Set the alpha (transparency) component of the color
+        mySprite.color = newColor;
+    }
+
+    private void ResetOpacity()
+    {
+        mySprite.color = originalColor; // Reset the color to the original color
+    }
+}
