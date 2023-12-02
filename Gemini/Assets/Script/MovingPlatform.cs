5252912f5d82f73cd46c4b315dc26aadeb1f738a
Konouz Abdelaziz <konozabdelaziz1@gmail.com>
2023-12-02 20:29:40 +0200

    koki small fixes

--- Gemini/Assets/Script/MovingPlatform.cs
+++ Gemini/Assets/Script/MovingPlatform.cs
@@ -27,7 +27,7 @@ public class MovingPlatform : MonoBehaviour
             collision.gameObject.transform.SetParent(transform);
         }
     }
-    private void OnCollisionExit2D(Collider2D collision)
+    private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
