64593dc160d7c21f258e5dc224c4a7f1a1eb418c
YaraMasoed <yaramasoed6@gmail.com>
2023-12-06 21:22:29 +0200

    ....2

---
+++ Gemini/Assets/Script/Treadmill.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Treadmill : MonoBehaviour
+{
+    public float treadmillSpeed = 3f; // Adjust the treadmill speed as needed
+
+    void Update()
+    {
+        // Move the player to the left (treadmill direction) only if it's on the treadmill
+        if (transform.childCount > 0)
+        {
+            Transform player = transform.GetChild(0); // Assuming the player is the first child
+            player.Translate(Vector3.left * treadmillSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) // Assuming "Player" is the tag of the player GameObject
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
