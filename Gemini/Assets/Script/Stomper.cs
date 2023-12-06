fc163a51a3c6d6d6952b7037b90400e89de6c63c
meribue2004 <151452615+meribue2004@users.noreply.github.com>
2023-12-06 10:23:21 +0200

    Stommperadded

---
+++ Gemini/Assets/Script/Stomper.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stomper : MonoBehaviour
+{
+ 
+    public float amplitude = 1.0f;   
+    public float frequency = 1.0f;
+    public bool stomper2 = false;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+        if (stomper2)
+        {
+
+            Invoke("MoveUpDown", 8.0f);
+        }
+        
+
+        StartCoroutine(MoveUpDown());
+    }
+
+    IEnumerator MoveUpDown() { 
+   
+
+while (true)
+        {
+            
+            float newY = startPos.y + amplitude * Mathf.Sin(frequency * Time.time);
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+            yield return null;
+        }
+    }
+}
