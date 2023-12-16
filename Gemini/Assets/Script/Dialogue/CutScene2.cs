709af67d3650bf855b2e0e23f3fc1340d8433eed
YaraMasoed <yaramasoed6@gmail.com>
2023-12-16 21:28:20 +0200

    eva caught in 4k

--- Gemini/Assets/Script/Dialogue/CutScene2.cs
+++ Gemini/Assets/Script/Dialogue/CutScene2.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class CutScene2 : MonoBehaviour
 {
-    public DialogueManager dialogueManager;
+    public DialogueManagerCutScene dialogueManager;
+    public bool isTriggered;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isTriggered = false;
     }
 
     // Update is called once per frame
@@ -20,14 +21,14 @@ public class CutScene2 : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && !isTriggered)
         {
+            isTriggered = true;
             string[] dialogue =
             {
                 "Eva: Phew! that was close! ",
                 "Adam: Now, where is the control room?",
                 "Eva: The control room should be at the end of this hall. Once we get there, I will connect the wires to deactivate your tracking bracelet. ",
-                "*Siren Triggeres*",
                 "Worried Eva: Oh no! They spotted us! Quick we need to hurry!",
                 "Adam: Then what are we waiting for? let's go!",
                 
