d7161ea44a4db4de188848a51f1900026122fe40
YaraMasoed <yaramasoed6@gmail.com>
2023-12-16 21:13:14 +0200

    grand pa lu cut scene finished

--- Gemini/Assets/Script/Dialogue/CutScene1.cs
+++ Gemini/Assets/Script/Dialogue/CutScene1.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CutScene1 : MonoBehaviour
 {
-    public DialogueManager dialogueManager;
+    public DialogueManagerCutScene dialogueManager;
+    public bool isTriggered;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isTriggered = false;
     }
 
     // Update is called once per frame
@@ -19,18 +21,19 @@ public class CutScene1 : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && !isTriggered)
         {
+            isTriggered = true;
             string[] dialogue =
             {
-                "Grandpa Lu: As you already know Adam, Earth hasn't healed yet..But that is not the truth!!",
+                "Lu: As you already know Adam, Earth hasn't healed yet..But that is not the truth!!",
                 "Adam: Whoa, what do you mean?",
-                "Grandpa Lu: 40 years ago,me and scientist collegues have discovered the earth has finally healed and we can go back to planet Earth, But Gemini wasn't too happy about it..",
-                "Grandpa Lu: Gemini wanted to stay in control of the ship and humans, and so he decided to keep that information to itself by wiping away the memory of my collegues.",
-                "Grandpa Lu: Luckily, I managed to evade it at the last minute.",
-                "Adam:We won't let that happen again, Grandpa. We will tell them the truth this time and send those people back to their homeland. ",
-                "Grandpa Lu: But it won't be easy, Adam. You must disable Gemini's control of the ship by taking the control chip and turning it manually.",
-                "Grandpa Lu: but first, you need to deactivate your tracking bracelet.",
+                "Lu: 40 years ago,me and scientist collegues have discovered the earth has finally healed and we can go back to planet Earth, But Gemini wasn't too happy about it..",
+                "Lu: Gemini wanted to stay in control of the ship and humans, and so he decided to keep that information to itself by wiping away the memory of my collegues.",
+                "Lu: Luckily, I managed to evade it at the last minute.",
+                "Adam: We won't let that happen again, Grandpa. We will tell them the truth this time and send those people back to their homeland. ",
+                "Lu: But it won't be easy, Adam. You must disable Gemini's control of the ship by taking the control chip and turning it manually.",
+                "Lu: but first, you need to deactivate your tracking bracelet.",
                 "Adam: How can I do it?",
                 "Eva: Don't worry Adam, I can handle it and i will help you through your mission.",
                 "Adam: Then let's get going, Eva.",
