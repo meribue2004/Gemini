6df4cddc0a0bbaae33fbf25bdf2dbe704ab33ace
meribue2004 <151452615+meribue2004@users.noreply.github.com>
2023-12-17 01:26:39 +0200

    Update Wire.cs

--- Gemini/Assets/PuzzleWires/Wire.cs
+++ Gemini/Assets/PuzzleWires/Wire.cs
@@ -1,4 +1,96 @@
-﻿using System.Collections;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Wire : MonoBehaviour
+//{
+//    public SpriteRenderer wireEnd;
+//    public GameObject lightOn;
+//    Vector3 startPoint;
+//    Vector3 startPosition;
+//    // Start is called before the first frame update
+
+
+//    void Start()
+//    {
+//        startPoint = transform.parent.position;
+//        startPosition = transform.position;
+
+
+//        // Retrieve the starting position
+//        string startPositionString = PlayerPrefs.GetString("StartPosition");
+//        Vector3 start = Vector3.zero;
+
+
+//    }
+
+//    private void OnMouseDrag()
+//    {
+//        // mouse position to world point
+//        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//        newPosition.z = 0;
+
+//        // check for nearby connection points
+//        Collider2D[] colliders = Physics2D.OverlapCircleAll(newPosition, .2f);
+//        foreach (Collider2D collider in colliders)
+//        {
+//            // make sure not my own collider
+//            if (collider.gameObject != gameObject)
+//            {
+//                // update wire to the connection point position
+//                UpdateWire(collider.transform.position);
+
+//                // check if the wires are same color
+//                if (transform.parent.name.Equals(collider.transform.parent.name))
+//                {
+//                    // count connection
+//                    Main.Instance.SwitchChange(1);
+
+//                    // finish step
+//                    collider.GetComponent<Wire>()?.Done();
+//                    Done();
+//                }
+//                return;
+//            }
+//        }
+
+//        // update wire
+//        UpdateWire(newPosition);
+//    }
+
+//    void Done()
+//    {
+//        // turn on light
+//        lightOn.SetActive(true);
+
+//        // destory the script
+//        Destroy(this);
+
+//    }
+
+//    private void OnMouseUp()
+//    {
+//        // reset wire position
+//        UpdateWire(startPosition);
+//    }
+
+//    void UpdateWire(Vector3 newPosition)
+//    {
+//        // update position
+//        transform.position = newPosition;
+
+//        // update direction
+//        Vector3 direction = newPosition - startPoint;
+//        transform.right = direction * transform.lossyScale.x;
+
+//        // update scale
+//        float dist = Vector2.Distance(startPoint, newPosition);
+//        wireEnd.size = new Vector2(dist, wireEnd.size.y);
+
+//    }
+
+//}
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,85 +100,64 @@ public class Wire : MonoBehaviour
     public GameObject lightOn;
     Vector3 startPoint;
     Vector3 startPosition;
-    // Start is called before the first frame update
-
 
     void Start()
     {
         startPoint = transform.parent.position;
         startPosition = transform.position;
-
-
-        // Retrieve the starting position
-        string startPositionString = PlayerPrefs.GetString("StartPosition");
-        Vector3 start = Vector3.zero;
-
-
     }
 
     private void OnMouseDrag()
     {
-        // mouse position to world point
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         newPosition.z = 0;
 
-        // check for nearby connection points
         Collider2D[] colliders = Physics2D.OverlapCircleAll(newPosition, .2f);
         foreach (Collider2D collider in colliders)
         {
-            // make sure not my own collider
-            if (collider.gameObject != gameObject)
+            if (collider != null && collider.gameObject != null && collider.gameObject != gameObject)
             {
-                // update wire to the connection point position
                 UpdateWire(collider.transform.position);
 
-                // check if the wires are same color
-                if (transform.parent.name.Equals(collider.transform.parent.name))
+                if (collider.transform != null && collider.transform.parent != null && transform.parent != null &&
+                    transform.parent.name.Equals(collider.transform.parent.name))
                 {
-                    // count connection
                     Main.Instance.SwitchChange(1);
 
-                    // finish step
-                    collider.GetComponent<Wire>()?.Done();
-                    Done();
+                    Wire wireComponent = collider.GetComponent<Wire>();
+                    if (wireComponent != null)
+                    {
+                        wireComponent.Done();
+                    }
+
+                    Done(); // Call Done() method in the current script
                 }
                 return;
             }
         }
 
-        // update wire
+
         UpdateWire(newPosition);
     }
 
     void Done()
     {
-        // turn on light
         lightOn.SetActive(true);
-
-        // destory the script
         Destroy(this);
-
     }
 
     private void OnMouseUp()
     {
-        // reset wire position
         UpdateWire(startPosition);
     }
 
     void UpdateWire(Vector3 newPosition)
     {
-        // update position
         transform.position = newPosition;
-
-        // update direction
         Vector3 direction = newPosition - startPoint;
         transform.right = direction * transform.lossyScale.x;
-
-        // update scale
         float dist = Vector2.Distance(startPoint, newPosition);
         wireEnd.size = new Vector2(dist, wireEnd.size.y);
-
     }
-
 }
+
