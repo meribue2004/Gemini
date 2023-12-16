fd36207cf5a591b4a6953f5449b8b4667758b5c9
meribue2004 <151452615+meribue2004@users.noreply.github.com>
2023-12-16 10:42:11 +0200

    update

--- Gemini/Assets/Script/GuideLevel1.cs
+++ Gemini/Assets/Script/GuideLevel1.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class GuideLevel1 : MonoBehaviour
 {
     public DialogueManager dialogueManager;
+    public bool isTriggered;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isTriggered = false;
     }
 
     // Update is called once per frame
@@ -19,8 +20,9 @@ public class GuideLevel1 : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && !isTriggered)
         {
+            isTriggered = true;
             string[] dialogue =
             {
                 "Eva: Avoid camera survieliance by going in the opposite direction. this way Gemini won't catch you.",
