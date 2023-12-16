f4c67cc1820eb21655d522df0c7237b402d298cb
iamAlLaa <151520600+JustSaraH02@users.noreply.github.com>
2023-12-16 12:59:26 +0200

    Level 1 completly complete

--- Gemini/Assets/Script/GuideLevel1.cs
+++ Gemini/Assets/Script/GuideLevel1.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class GuideLevel1 : MonoBehaviour
 {
     public DialogueManager dialogueManager;
+    public bool isTriggered;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isTriggered = false;
     }
 
     // Update is called once per frame
@@ -19,13 +20,14 @@ public class GuideLevel1 : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && !isTriggered)
         {
+            isTriggered = true;
             string[] dialogue =
             {
                 "Eva: Avoid camera survieliance by going in the opposite direction. this way Gemini won't catch you.",
                 "Eva: Collect keys along the way to unlock the control room.",
-                "Eva: Yikes! avoid the spikes.",
+                "Worried Eva: Yikes! avoid the spikes.",
                 "Eva: Good Luck!",
             };
             dialogueManager.SetSentences(dialogue);
