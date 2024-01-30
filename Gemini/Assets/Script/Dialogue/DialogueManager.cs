e9aef7ec7090a42f2e1ceb5b309d41b272062f92
Konouz Abdelaziz <konozabdelaziz1@gmail.com>
2024-01-30 23:31:38 +0200

    added dialogue to all

--- Gemini/Assets/Script/Dialogue/DialogueManager.cs
+++ Gemini/Assets/Script/Dialogue/DialogueManager.cs
@@ -56,7 +56,7 @@ public class DialogueManager : MonoBehaviour
             // Adjust the size of the Image component based on the sprite size
             characterImage.rectTransform.sizeDelta = new Vector2(characterSprite.rect.width, characterSprite.rect.height);
         }
-
+       GetSoundForCharacter(characterName);
         foreach (char letter in dialogueSentence[index].ToCharArray())
         {
             textDisplay.text += letter;
@@ -65,11 +65,44 @@ public class DialogueManager : MonoBehaviour
 
             if (textDisplay.text == dialogueSentence[index])
             {
+                 Eskot();
                 continueButton.SetActive(true);
             }
         }
     }
-
+  private void Eskot(){
+    FindObjectOfType<AudioManager>().Stop("dialogue");
+            FindObjectOfType<AudioManager>().Stop("dialogue1");
+            FindObjectOfType<AudioManager>().Stop("dialogue2");
+            FindObjectOfType<AudioManager>().Stop("dialoguerandom");
+}
+private void GetSoundForCharacter(string characterName)
+{
+    // You can use a switch statement or other logic to map character names to sound names
+    switch (characterName)
+    {
+        case "Lu":
+           FindObjectOfType<AudioManager>().Play("dialogue1");
+           break; // Adjust this to the actual sound name you want to play for "meme" character.
+        case "Adam":
+             FindObjectOfType<AudioManager>().Play("dialogue2");
+             break;
+        // Add more cases as needed for different characters
+        case "Eva":
+             FindObjectOfType<AudioManager>().Play("dialogue"); 
+             break;
+        case "Worried Eva":
+             FindObjectOfType<AudioManager>().Play("dialogue"); 
+             break;
+        case "Scared Eva":
+             FindObjectOfType<AudioManager>().Play("dialogue"); 
+             break;
+        case "Evil Eva":
+             FindObjectOfType<AudioManager>().Play("dialogue"); 
+             break;
+        // Default sound if the character name is not recognized
+    }
+}
 
     public void SetSentences(string[] sentences)
     {
