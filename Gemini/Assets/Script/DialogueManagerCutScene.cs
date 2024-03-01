1c1861236c85572eaac1937e38bfcda5e41a929b
YaraMasoed <yaramasoed6@gmail.com>
2024-03-01 12:44:56 +0200

    dialogue and level 3 door

--- Gemini/Assets/Script/DialogueManagerCutScene.cs
+++ Gemini/Assets/Script/DialogueManagerCutScene.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 
 public class DialogueManagerCutScene : MonoBehaviour
 {
+
     public TextMeshProUGUI textDisplay;
     private string[] dialogueSentence;
     private int index = 0;
@@ -40,74 +41,14 @@ public class DialogueManagerCutScene : MonoBehaviour
         characterImages.Add("Lu", Lu);
         dialogueBox.SetActive(false);
     }
-public IEnumerator TypeDialogue()
-{
-    
-    isDialogueActive = true;
-    dialogueBox.SetActive(true);
-
-    // Setting the char image according to the name
-    string characterName = dialogueSentence[index].Split(':')[0].Trim();
-    if (characterImages.ContainsKey(characterName))
+    public IEnumerator TypeDialogue()
     {
-        Sprite characterSprite = characterImages[characterName];
-        characterImage.sprite = characterSprite;
 
-        // Adjust the size of the Image component based on the sprite size
-        characterImage.rectTransform.sizeDelta = new Vector2(characterSprite.rect.width, characterSprite.rect.height);
-    }
 
-     GetSoundForCharacter(characterName);
-    
-
-    foreach (char letter in dialogueSentence[index].ToCharArray())
-    {
-        textDisplay.text += letter;
-       
-
-        yield return new WaitForSeconds(typingSpeed);
-
-        if (textDisplay.text == dialogueSentence[index])
-        {
-             Eskot();
-            Invoke("NextSentence", 0.8f);
-        }
-    }
-}
-private void Eskot(){
-    FindObjectOfType<AudioManager>().Stop("dialogue");
-            FindObjectOfType<AudioManager>().Stop("dialogue1");
-            FindObjectOfType<AudioManager>().Stop("dialogue2");
-            FindObjectOfType<AudioManager>().Stop("dialoguerandom");
-}
-private void GetSoundForCharacter(string characterName)
-{
-    // You can use a switch statement or other logic to map character names to sound names
-    switch (characterName)
-    {
-        case "Lu":
-           FindObjectOfType<AudioManager>().Play("dialogue1");
-           break; // Adjust this to the actual sound name you want to play for "meme" character.
-        case "Adam":
-             FindObjectOfType<AudioManager>().Play("dialogue2");
-             break;
-        // Add more cases as needed for different characters
-        case "Eva":
-             FindObjectOfType<AudioManager>().PlayRandom("dialogue","dialoguerandom"); 
-             break;
-        // Default sound if the character name is not recognized
-    }
-}
-
-   /* public IEnumerator TypeDialogue()
-    {
-         
-        FindObjectOfType<AudioManager>().Play("meme");
         isDialogueActive = true;
-
         dialogueBox.SetActive(true);
 
-        //setting the char image according to the name
+        // Setting the char image according to the name
         string characterName = dialogueSentence[index].Split(':')[0].Trim();
         if (characterImages.ContainsKey(characterName))
         {
@@ -118,19 +59,48 @@ private void GetSoundForCharacter(string characterName)
             characterImage.rectTransform.sizeDelta = new Vector2(characterSprite.rect.width, characterSprite.rect.height);
         }
 
+        GetSoundForCharacter(characterName);
+
+
         foreach (char letter in dialogueSentence[index].ToCharArray())
         {
             textDisplay.text += letter;
+
 
             yield return new WaitForSeconds(typingSpeed);
 
             if (textDisplay.text == dialogueSentence[index])
             {
+                Eskot();
                 Invoke("NextSentence", 0.8f);
             }
         }
     }
-*/
+    private void Eskot()
+    {
+        FindObjectOfType<AudioManager>().Stop("dialogue");
+        FindObjectOfType<AudioManager>().Stop("dialogue1");
+        FindObjectOfType<AudioManager>().Stop("dialogue2");
+        FindObjectOfType<AudioManager>().Stop("dialoguerandom");
+    }
+    private void GetSoundForCharacter(string characterName)
+    {
+        // You can use a switch statement or other logic to map character names to sound names
+        switch (characterName)
+        {
+            case "Lu":
+                FindObjectOfType<AudioManager>().Play("dialogue1");
+                break; // Adjust this to the actual sound name you want to play for "meme" character.
+            case "Adam":
+                FindObjectOfType<AudioManager>().Play("dialogue2");
+                break;
+            // Add more cases as needed for different characters
+            case "Eva":
+                FindObjectOfType<AudioManager>().PlayRandom("dialogue", "dialoguerandom");
+                break;
+                // Default sound if the character name is not recognized
+        }
+    }
 
     public void SetSentences(string[] sentences)
     {
@@ -147,7 +117,7 @@ private void GetSoundForCharacter(string characterName)
         }
         else
         {
-            
+
             textDisplay.text = "";
             dialogueBox.SetActive(false);
             this.dialogueSentence = null;
@@ -155,8 +125,9 @@ private void GetSoundForCharacter(string characterName)
             player.constraints = RigidbodyConstraints2D.None;
             player.constraints = RigidbodyConstraints2D.FreezeRotation;
             isDialogueActive = false;
-            if (!Ending1) {
-                Invoke("load", 0.3f); 
+            if (!Ending1)
+            {
+                Invoke("load", 0.3f);
             }
         }
     }
@@ -166,7 +137,5 @@ private void GetSoundForCharacter(string characterName)
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
-
 }
-
 
